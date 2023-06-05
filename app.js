31111e3c28226d18570cd68b4e83a0516cb0d72a
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-06-05 14:49:16 -0300

    exibindo dados da api de clima no browser

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
  //jshint esversion:6
- const { log } = require('console');
-const express = require('express');
+ const express = require('express');
  const app = express();
  const https = require('https');
 
  app.get("/", function(req, res) {
-   const url = "https://api.openweathermap.org/data/2.5/weather?q=Recife&appid=e753e23fb530ad0639c43c4c4b4f2e29&units=metric";
+   const city = "Limoeiro";
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e753e23fb530ad0639c43c4c4b4f2e29&units=metric`;
    https.get(url, function(response) {
       console.log(response.statusCode);
       response.on("data", function(data) {
@@ -13,9 +13,15 @@ const express = require('express');
          console.log(weatherData);
          const temp = weatherData.main.temp;
          const descrip = weatherData.weather[0].description;
+         const icon = weatherData.weather[0].icon;
+         const urlIcon = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+         res.write("<p> Weather: " + descrip + "</p>");
+         res.write("<h1>The temperature in " + city + " is: " + temp + " graus Celsius.</h1>");
+         res.write(`<img src=${urlIcon}>`);
+         res.send();
       })
    })
-   res.send("Server is up and running")
+
  })
 
  app.listen(3000, function() {
