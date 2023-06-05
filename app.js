658cda4460ef40bbda21fa2ec9a8d2fd4fb4f90c
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-06-05 12:58:40 -0300

    utilizando o método on do módulo https nativo

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
  //jshint esversion:6
- const express = require('express');
+ const { log } = require('console');
+const express = require('express');
  const app = express();
  const https = require('https');
 
  app.get("/", function(req, res) {
    const url = "https://api.openweathermap.org/data/2.5/weather?q=Recife&appid=e753e23fb530ad0639c43c4c4b4f2e29&units=metric";
    https.get(url, function(response) {
-      console.log("The response is" + response)
+      console.log(response.statusCode);
+      response.on("data", function(data) {
+         const weatherData = JSON.parse(data);
+         console.log(weatherData);
+         const temp = weatherData.main.temp;
+         const descrip = weatherData.weather[0].description;
+      })
    })
    res.send("Server is up and running")
  })
